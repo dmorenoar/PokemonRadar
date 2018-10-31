2c093061c6e5af18fe1c3ce6358b3955a6fc7f4b
Daniel Moreno Arellano <danielmorenoarellano@gmail.com>
2018-10-31 17:03:15 +0100

    aaa

--- PokemonRadar/AppDelegate.swift
+++ PokemonRadar/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyBk8xLqzMqr2ZugQHLbEqfJMSF11tsysbQ")
+        GMSPlacesClient.provideAPIKey("GMSPlacesClient.provideAPIKey")
+        
+        
         return true
     }
 
